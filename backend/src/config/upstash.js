f49a4a044303ef2be36e7a1193f1d8a4c7f37e45
codeIshan <ishan5bala4@gmail.com>
2025-08-27 18:16:05 +0530

    Initial Commit

---
+++ backend/src/config/upstash.js
@@ -0,0 +1,15 @@
+
+
+import {Ratelimit} from "@upstash/ratelimit"
+import { Redis } from "@upstash/redis"
+import dotenv from "dotenv"
+
+dotenv.config();
+
+//create ratelimit for 10 req per 20 secs
+const ratelimit = new Ratelimit({
+    redis: Redis.fromEnv(),
+    limiter: Ratelimit.slidingWindow(10, "20 s")
+});
+
+export default ratelimit;
---
+++ backend/src/server.js
@@ -0,0 +1,32 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+
+import notesRoutes from "./routes/notesRoutes.js";
+import { connectDB } from "./config/db.js";
+import rateLimiter from "../middleware/rateLimiter.js";
+
+dotenv.config();
+const PORT = process.env.PORT || 5001
+
+const app = express();
+
+//middleware
+app.use(cors({
+    origin:"http://localhost:5174",
+}));
+app.use(express.json());
+app.use(rateLimiter);
+
+app.use((req,res,next) => {
+    console.log("We just got a new request");
+    next();
+});
+
+app.use("/api/notes", notesRoutes);
+
+connectDB().then(() => {
+    app.listen(PORT, () => {
+        console.log("Server started on Port: ", PORT);
+    });
+});
